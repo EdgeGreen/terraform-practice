229c8d7d4b1a9eecc810fe2a421f07687cd89083
Ihor Blyzniuk <62443956+EdgeGreen@users.noreply.github.com>
2023-04-15 19:21:05 +0300

    fix pipeline

--- backend/s3.tf
+++ backend/s3.tf
@@ -1,18 +1,18 @@
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-practice-green"
+  bucket = "terraform-practice-green"
 
-    # lifecycle {
-    #     prevent_destroy = true
-    #    }
+  # lifecycle {
+  #     prevent_destroy = true
+  #    }
 
-    versioning {
-        enabled = true
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
-       server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-            }
-        }
-    }
+  }
 }
